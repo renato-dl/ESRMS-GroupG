ec1cfc563b1d5e5dd8048a5750c767dd12919624
renato-dl <s263092@studenti.polito.it>
2019-11-11 11:26:36 +0100

    Updated grade model

--- src/database/models/grade.js
+++ src/database/models/grade.js
@@ -4,14 +4,18 @@ class Grade extends Model {
   constructor() {
     super('Grades');
   }
-  async findByStudentId(studentId) {
+  async findByStudentId(parentId, studentId) {
     const connection = await this.db.getConnection();
     const results = await connection.query(
         `SELECT Subjects.Name, Grade, Date
-        FROM ${this.tableName}, Subjects
-        WHERE ${this.tableName}.SubjectId = Subjects.ID AND StudentId = ?
+        FROM ${this.tableName}, Subjects, Students
+        WHERE
+          ${this.tableName}.SubjectId = Subjects.ID AND
+          ${this.tableName}.StudentId = Student.ID AND
+          StudentId = ? AND
+          (Parent1 = ? OR Parent2 = ?)
         ORDER BY Date DESC`,
-        [studentId]
+        [studentId, parentId, parentId]
     );
     connection.release();
 
