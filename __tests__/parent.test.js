907a4e0d5c49b86470d50ddf5d1dd06fa0dca10d
Xhoi Kerbizi <s257579@studenti.polito.it>
2019-11-12 14:10:36 +0100

    updated test message

--- __tests__/parent.test.js
+++ __tests__/parent.test.js
@@ -2,8 +2,8 @@ import Grade from "../src/database/models/grade";
 
 describe('Customer tests', () => {
    
-    test('It should check the correctness of accounting service time', async () => {
-        const grades = await Grade.findByStudentId(1, 1);
+    test('It should retrive the grades of that student', async () => {
+        const grades = await Grade.findByStudentId("bcbcbcbcbc", "266667153e975bbf735b89d4b03d9f93");
         console.log(grades);
         expect(5).toBe(5);
 
