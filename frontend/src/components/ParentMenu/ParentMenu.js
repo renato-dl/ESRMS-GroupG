18a1c4596f822a648976492e77a2091b39a36671
Mara-M <maramarmaryan@gmail.com>
2019-11-15 17:05:54 +0100

    solved sidebar link problem

--- frontend/src/components/ParentMenu/ParentMenu.js
+++ frontend/src/components/ParentMenu/ParentMenu.js
@@ -4,50 +4,33 @@ import {Link} from 'react-router-dom';
 
 export const ParentMenu = (props) => (
   <>
-    <Menu.Item className="menuChildItem">
-      <Link to="/parent">
-        <span>
-          <br/>
-          <Icon name='child' size="big" />
-          ChildName 
-          <br/>
-          <br/>
-        </span>
-        {/* <span>
-          <Icon name='user circle big' />
-          Student
-          <br/><br/>
-          Name Surname
-          <br/>
-        </span> */}
-      </Link>
-      
+    <Menu.Item as={Link} to="/parent">
+      <span>
+        <br/>
+        <Icon name='child' size="big" />
+        ChildName 
+        <br/><br/>
+      </span>
     </Menu.Item>
 
-    <Menu.Item className="menuStandard">
-      <Link to="/student/:studentID/marks">
-        <Icon name='sort numeric up' />
-        Grades
-      </Link>
+    <Menu.Item as={Link} to='/student/:studentID/marks'>
+      <Icon name='sort numeric up' />
+      Grades
     </Menu.Item>
-    <Menu.Item className="menuStandard">
-      <Link to="/student/:studentID/homeworks">
-        <Icon name='home' />
-        Homeworks
-      </Link>
+
+    <Menu.Item as={Link} to="/student/:studentID/homeworks">
+      <Icon name='home'/>
+      Homeworks
     </Menu.Item>
     
-    <Menu.Item className="menuStandard">
-      <Link to="/student/:studentID/homeworks">
-        <Icon name='calendar check outline' />
-        Attendance
-      </Link>
+    <Menu.Item as={Link} to="/student/:studentID/homeworks">
+      <Icon name='calendar check outline'/>
+      Attendance
     </Menu.Item>
-    <Menu.Item className="menuStandard">
-      <Link to="/student/:studentID/homeworks">
-        <Icon name='file alternate outline' />
-        Study Plan
-      </Link>
+
+    <Menu.Item as={Link} to="/student/:studentID/homeworks">
+      <Icon name='file alternate outline'/>
+      Study Plan
     </Menu.Item>
   </>
 );
