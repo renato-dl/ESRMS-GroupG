9f5fc613083819da3d77e8ff93129cb26dacda25
xhoi <xhoikerbizi@gmail.com>
2019-11-18 16:08:00 +0100

    Error in adminController

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -28,7 +28,7 @@ class AdminController extends BaseController {
       req.body.SSN, 
       password
     );
-    this.sendEmailToParent(eMail, password, firstName, lastName);
+    this.sendEmailToParent(req.body.eMail, password, firstName, lastName);
     res.send(parent); 
   }
 
