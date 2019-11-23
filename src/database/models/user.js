93af33067841a9052f55b78bdf4ae36f7fe8ab7f
renato-dl <s263092@studenti.polito.it>
2019-11-23 11:54:34 +0100

    Created user role query method

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -4,6 +4,15 @@ class User extends Model {
   constructor() {
     super('users');
   }
+  async getUserRolesById(userId) {
+    const connection = await this.db.getConnection();
+    return connection.query(
+      `SELECT IsParent, IsTeacher, IsPrincipal , IsAdminOfficer, IsSysAdmin
+      FROM Users
+      WHERE ID = ?;`,
+      [userId]
+    )
+  }
 }
 
 export default new User();
