42daabf96717aff171f22afd65abd8d2ad15b214
renato-dl <s263092@studenti.polito.it>
2019-11-23 13:04:25 +0100

    Updated parent data insertion

--- src/database/models/parent.js
+++ src/database/models/parent.js
@@ -28,12 +28,9 @@ class Parent extends Model {
     return results;
   }
 
-  async insertParentData(adminId, firstName, lastName, eMail, SSN, password) {
+  async insertParentData(firstName, lastName, eMail, SSN, password) {
 
       //input data validation
-      if (!adminId){
-        throw new Error('Missing or invalid admin Id');
-      }
       if (!validator.matches(firstName,'^[a-zA-Z]+( [a-zA-Z]+)*$')) {
         throw new Error('Missing or invalid first name');
       }
@@ -48,62 +45,43 @@ class Parent extends Model {
       }
       
     const connection = await this.db.getConnection();
-    
-    
-    const selectResult = await connection.query(
-      `SELECT *
-      FROM Users
-      WHERE eMail = ?;`,
-      [eMail]
-    );
 
-    if (selectResult.length != 0) {
-      if (selectResult[0].isParent) {
-        throw new Error('Parent already in db');
+    try {
+      const selectResult = await connection.query(
+        `SELECT *
+        FROM Users
+        WHERE SSN = ? OR eMail = ?;`,
+        [SSN, eMail]
+      );
+
+      if (selectResult.length != 0) {
+        connection.release();
+        throw new Error('User already in db')
+      }
+
+      //insert of data
+      const parentId = crypto.createHash('sha256').update(eMail).digest('hex');
+      const parentPassword = createSecurePassword(password);
+
+      const insertResult = await connection.query(
+        `INSERT INTO Users (ID, eMail, Password, IsParent, FirstName, LastName, SSN)
+        VALUES (?, ?, ?, true, ?, ?, ?);`,
+        [parentId, eMail, parentPassword, firstName, lastName, SSN]
+      );
+      connection.release();
+
+      if (insertResult.affectedRows != 1) {
+        throw new Error('Something went wrong')
       } else {
-          const updateResult = await connection.query(
-            `UPDATE Users
-            SET IsParent = true
-            WHERE ID = ?;`,
-            [selectResult[0].ID]
-          );
-          connection.release();
-          if (updateResult.affectedRows != 1) {
-            return {
-              "Success": false,
-              "Operation": 'UPDATE'
-            }
-          }
-          return {
-            "Success": true,
-            "Operation": 'UPDATE'
-          }
-              
+        return {
+          id: parentId
+        }
       }
-    }
-
-    //insert of data
-    const parentId = crypto.createHash('sha256').update(eMail).digest('hex');
-    const parentPassword = createSecurePassword(password);
-
-
-    const insertResult = await connection.query(
-      `INSERT INTO Users (ID, eMail, Password, IsParent, FirstName, LastName, SSN)
-      VALUES (?, ?, ?, true);`,
-      [parentId, eMail, parentPassword, firstName, lastName, SSN]
-    );
-    connection.release();
-    
-    if (updateResult.affectedRows != 1) {
-      return {
-        "Success": false,
-        "Operation": 'INSERT'
-      }
-    }
-    
-    return {
-      "Success": true,
-      "Operation": 'INSERT'
+      
+    } catch(err) {
+      connection.release();
+      console.log(err.message);
+      throw(err);
     }
   }
 
