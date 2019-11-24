f07bf4b5e92b82d64c4037198bcaff4bb6b38199
xhoi <xhoikerbizi@gmail.com>
2019-11-24 17:17:16 +0100

    removed authorization in  add topic

--- src/routes/teacher.js
+++ src/routes/teacher.js
@@ -5,7 +5,7 @@ import { Authorization } from '../middlewares/authorization';
 const router = express.Router();
 
 router.get('/subjects',  Authorization(['IsTeacher']), TeacherController.processRequest.bind(TeacherController, 'subjectsByTeacherId'));
-router.post('/topic', Authorization(['IsTeacher']), TeacherController.processRequest.bind(TeacherController, 'addTopic'));
+router.post('/topic', TeacherController.processRequest.bind(TeacherController, 'addTopic'));
 router.delete('/topic', TeacherController.processRequest.bind(TeacherController, 'deleteTopic'));
 router.patch('/topic', Authorization(['IsTeacher']), TeacherController.processRequest.bind(TeacherController, 'patchTopic'));
 router.get('/topics', Authorization(['IsTeacher']), TeacherController.processRequest.bind(TeacherController, 'topicsByTeacherClassSubject'));
