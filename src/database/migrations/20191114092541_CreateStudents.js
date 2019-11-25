210dbe0575ecaf1ac1ba1516bb132d1d8dbeef82
renato-dl <s263092@studenti.polito.it>
2019-11-25 16:37:46 +0100

    Updated migrations

--- src/database/migrations/20191114092541_CreateStudents.js
+++ src/database/migrations/20191114092541_CreateStudents.js
@@ -1,14 +1,14 @@
 
 exports.up = function(knex) {
   return knex.schema.createTable('Students', function (table) {
-    table.string('ID', 50).primary();
+    table.string('ID').primary();
     table.string('FirstName', 30).notNullable();
     table.string('LastName', 30).notNullable();
     table.string('SSN', 16).unique().notNullable();
     table.timestamp('BirthDate').notNullable();
-    table.string('Parent1', 50).notNullable();
-    table.string('Parent2', 50).nullable().defaultTo(null);
-    table.integer('ClassId').unsigned().notNullable();
+    table.string('Parent1').notNullable();
+    table.string('Parent2').nullable().defaultTo(null);
+    table.integer('ClassId').unsigned();
     table.timestamp('CreatedOn').defaultTo(knex.fn.now()).notNullable();
 
     table.foreign('Parent1').references('Users.ID');
--- src/database/migrations/20191114093327_CreateGrades.js
+++ src/database/migrations/20191114093327_CreateGrades.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
   return knex.schema.createTable('Grades', function (table) {
     table.increments('ID').unsigned().primary();
     table.integer('SubjectId').unsigned().notNullable();
-    table.string('StudentId', 50).notNullable();
+    table.string('StudentId').notNullable();
     table.decimal('Grade', 10, 0).notNullable();
     table.timestamp('GradeDate').notNullable();
 
