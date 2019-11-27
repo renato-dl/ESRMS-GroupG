66d55ac7f4a50c6254a54dc5b24f0c3532201816
xhoi <xhoikerbizi@gmail.com>
2019-11-27 09:57:23 +0100

    added route for update parent

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.post('/student', Authorization(['IsAdminOfficer']), AdminController.processRequest.bind(AdminController, 'addStudent'));
 router.patch('/student', Authorization(['IsAdminOfficer']), AdminController.processRequest.bind(AdminController, 'updateStudent'));
+router.patch('/parent', Authorization(['IsAdminOfficer']), AdminController.processRequest.bind(AdminController, 'updateParent'));
 router.get('/parents', Authorization(['IsAdminOfficer']), AdminController.processRequest.bind(AdminController, 'getParentData'));
 router.get('/find-parents', Authorization(['IsAdminOfficer']), AdminController.processRequest.bind(AdminController, 'getParentsBySSN'));
 export default router;
