52e642d2b6cea1b5a7ef70b53abc592a62bb7bb5
xhoi <xhoikerbizi@gmail.com>
2019-11-27 17:41:47 +0100

    updated query with range optionality

--- src/controllers/parentController.js
+++ src/controllers/parentController.js
@@ -19,8 +19,13 @@ class ParentController extends BaseController {
     res.send(grades);
   }
   async assigmentsByStudentId(req, res) {
+    if (!await Student.checkIfRelated(req.query.studentId, req.user.ID)) {
+      res.send(401);
+      return;
+    }
     const assignments = await Assignment.findByStudentId(
       req.query.studentId, 
+      {from: req.query.fromDate, to: req.query.toDate},
       {page: req.query.page, pageSize: req.query.pageSize}
       );
     res.send(assignments);
--- src/database/models/assignment.js
+++ src/database/models/assignment.js
@@ -5,18 +5,26 @@ class Assignment extends Model {
     super('Assignments');
   }
 
-  async findByStudentId(studentId, pagination) {
+  async findByStudentId(studentId, dateRange, pagination) {
     if (!studentId) throw new Error('Missing or invalid student id');
 
     const connection = await this.db.getConnection();
-    let query =
-        `SELECT *
-        FROM ${this.tableName}, Students
-        WHERE
-          ${this.tableName}.ClassId = Students.ClassId AND
-          AND StudentId = ?
-        ORDER BY DueDate`;
-
+    let query;
+    if(dateRange){
+        query =`SELECT SU.Name, A.Title, A.Description, A.DueData  
+        FROM Assignments A, Students ST, Subjects SU
+        WHERE A.ClassId = ST.ClassId AND A.SubjectId = SU.ID AND ST.StudentId = ?
+        AND A.DueDate >= ? AND A.DueDate <= ?
+        ORDER BY A.DueDate`,
+        [studentId, dateRange.from, dateRange.to]
+    }
+    else{
+        query =`SELECT SU.Name, A.Title, A.Description, A.DueData  
+            FROM Assignments A, Students ST, Subjects SU
+            WHERE A.ClassId = ST.ClassId AND A.SubjectId = SU.ID AND ST.StudentId = ?
+            ORDER BY A.DueDate`,
+            [studentId]
+    }
     if (pagination) {
       query += ` ${this.db.getPaginationQuery(pagination)}`
     }
