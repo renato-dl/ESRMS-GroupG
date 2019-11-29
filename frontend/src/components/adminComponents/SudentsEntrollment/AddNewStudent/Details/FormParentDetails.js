07f7dfabbaa56a889baed9d91d873cbd56e388ee
Mara-M <maramarmaryan@gmail.com>
2019-11-29 17:35:07 +0100

    Search fields for Parent1 and Prent2

--- frontend/src/components/adminComponents/SudentsEntrollment/AddNewStudent/Details/FormParentDetails.js
+++ frontend/src/components/adminComponents/SudentsEntrollment/AddNewStudent/Details/FormParentDetails.js
@@ -28,11 +28,10 @@ resultRenderer.propTypes = {
 }
 //initialState for search field
 const initialState = {
-  activeIndex: 1,     //For Accordion
-  idP1:"",
+  idP1:"", 
   idP2:"",
-  p1_value: '',
-  p2_value:'',          //For Search: final value   TODO: make another one vor parent2
+  p1_value: '',       //For Search: final value for Parent 2
+  p2_value:'',        //For Search: final value for Parent 2
   isLoading: false,   //For Search: loading icon 
   results: [],        //For Search: filtered data
 }
@@ -42,6 +41,7 @@ let source = [];
 
 
 export class FormParentDetails extends Component {
+  state = {activeIndex: 1};   //For Accordion: not in initialState to not be closed during search
   state = initialState
  
   back = e => {
@@ -66,32 +66,54 @@ export class FormParentDetails extends Component {
 
   //------START SEARCH STUFF
   handleResultSelect = (e, { result }) => {
-    //(e.target.name === "p1_SSN") ? this.setState({idP1: result.ID, p1_value:result.SSN}) : this.setState({idP2: result.ID, p2_value:result.SSN})
-     this.setState({ 
-      value: result.SSN,
-      idP1: result.ID
-     }) 
+    
+    e.preventDefault();
+    //console.log([e.target.name]);
+    if(e.target.name === "P1"){
+      this.setState({ 
+        p1_value: result.SSN,
+        idP1: result.ID
+       }) 
+    }else if (e.target.name === "P2"){
+      this.setState({ 
+        p2_value: result.SSN,
+        idP2: result.ID
+       }) 
+    }
   }
   
   handleSearchChange = (e, { value }) => {
+    e.preventDefault();
 
-    this.setState({idP1:"", idP2:""});
+    //this.setState({idP1:"", idP2:""});
+    
     this.updateSearchOptions(value);
     
+    if(e.target.name === "P1"){
+      this.setState({idP1:""});
+      this.setState({ isLoading: true, p1_value:value })
+      setTimeout(() => {
+        if (this.state.p1_value.length < 1) return this.setState(initialState)
+        const re = new RegExp(_.escapeRegExp(this.state.p1_value), 'i')
+        const isMatch = (result) => re.test(result.SSN)
+        this.setState({
+          isLoading: false,
+          results: _.filter(source, isMatch),
+      })}, 300)
 
-    this.setState({ isLoading: true, p1_value:value })
+    }else if(e.target.name === "P2"){
+      this.setState({idP2:""});
+      this.setState({ isLoading: true, p2_value:value })
+      setTimeout(() => {
+        if (this.state.p2_value.length < 1) return this.setState(initialState)
+        const re = new RegExp(_.escapeRegExp(this.state.p2_value), 'i')
+        const isMatch = (result) => re.test(result.SSN)
+        this.setState({
+          isLoading: false,
+          results: _.filter(source, isMatch),
+      })}, 300) 
+    }
 
-    setTimeout(() => {
-      if (this.state.p1_value.length < 1) return this.setState(initialState)
-
-      const re = new RegExp(_.escapeRegExp(this.state.p1_value), 'i')
-      const isMatch = (result) => re.test(result.SSN)
-
-      this.setState({
-        isLoading: false,
-        results: _.filter(source, isMatch),
-      })
-    }, 300)
   }
   //------END SEARCH STUFF
 
@@ -110,7 +132,7 @@ export class FormParentDetails extends Component {
       //for sharing props with FormStudentDetails state
       const {values, handleChange} = this.props;
 
-      const { isLoading, p1_value, results } = this.state
+      const { isLoading, p1_value, p2_value, results } = this.state
 
         return (
             <>
@@ -122,14 +144,15 @@ export class FormParentDetails extends Component {
             <Form.Field>
             <Grid>
               <Grid.Column>
-                  <label>SSN</label>
+                  <label><b>SSN</b></label>
                 <Search
-                  name="p1_SSN"
+                  name="P1"
                   loading={isLoading}
                   onResultSelect={this.handleResultSelect}
                   onSearchChange={_.debounce(this.handleSearchChange, 500, {
                     leading: true,
                   })}
+                  noResultsMessage = "No Parent Found."
                   //minCharacters = "4" //minimum characters to show options
                   results={results}
                   value={p1_value}
@@ -137,61 +160,41 @@ export class FormParentDetails extends Component {
                   {...this.props}
                 />
                 
-
-                {(!this.state.idP1.trim() == "") && <p style = {{color:'#68af64' }}><Icon name='check' />Details of this parent are known</p>}
+                {(!this.state.idP1.trim() == "") && <h5 style = {{color:'#68af64' }}><Icon name='check' />Details of this parent are known</h5>}
               </Grid.Column>
-              
-
-{/* 
---- VISUALIZE MY STATE 
---- NOTE: make prev. grid.column width={6} ----------------------------- 
-
-                <Grid.Column width={10}>
-                <Segment>
-                  <Header>State</Header>
-                  <pre style={{ overflowX: 'auto' }}>
-                    {JSON.stringify(this.state, null, 2)}
-                  </pre>
-                  <Header>Options</Header>
-                  <pre style={{ overflowX: 'auto' }}>
-                    {JSON.stringify(source, null, 2)}
-                  </pre> 
-                </Segment>
-                </Grid.Column>
-*/}
-
-
             </Grid>
             </Form.Field>
 
 
-            <Form.Group widths='equal'>
+            {(this.state.idP1.trim() == '') &&  
+            <>
+              <Form.Group widths='equal'>
+                <Form.Input
+                  label='First Name' placeholder='First Name'
+                  name='p1_FirstName'
+                  //disabled = {!this.state.idP1.trim() == ""}
+                  defaultValue = {values.p1_FirstName}
+                  onChange={handleChange('p1_FirstName')}
+                />
+                <Form.Input
+                  label='Last Name' placeholder='Last Name'
+                  name='p1_LastName'
+                  //disabled = {!this.state.idP1.trim() == ""}
+                  defaultValue = {values.p1_LastName}
+                  onChange={handleChange('p1_LastName')}
+                />
+              </Form.Group>
               <Form.Input
-                label='First Name' placeholder='First Name'
-                name='p1_FirstName'
-                disabled = {!this.state.idP1.trim() == ""}
-                //disabled={this.state.P1exists}
-                defaultValue = {values.p1_FirstName}
-                onChange={handleChange('p1_FirstName')}
+                  fluid icon='envelope' iconPosition='left' 
+                  label='E-mail address'type='email'placeholder='E-mail address'
+                  name="p1_Email"
+                  //disabled = {!this.state.idP1.trim() == ""}
+                  defaultValue = {values.p1_Email}
+                  onChange={handleChange('p1_Email')}
               />
-              <Form.Input
-                label='Last Name' placeholder='Last Name'
-                name='p1_LastName'
-                disabled = {!this.state.idP1.trim() == ""}
-                //disabled={this.state.P1exists}
-                defaultValue = {values.p1_LastName}
-                onChange={handleChange('p1_LastName')}
-              />
-            </Form.Group>
-            <Form.Input
-                fluid icon='envelope' iconPosition='left' 
-                label='E-mail address'type='email'placeholder='E-mail address'
-                name="p1_Email"
-                disabled = {!this.state.idP1.trim() == ""}
-                //disabled={this.state.P1exists}
-                defaultValue = {values.p1_Email}
-                onChange={handleChange('p1_Email')}
-            />
+            </>
+            }
+
 
 {/*--SECOND PARENT----------------------------------------------------------------------------*/}
             <Accordion as={Form.Field}  >
@@ -207,7 +210,31 @@ export class FormParentDetails extends Component {
 
             <Accordion.Content active={this.state.activeIndex === 0}>
                 <>
-                <Form.Group widths='equal'>
+                <Form.Field>
+                  <Grid>
+                    <Grid.Column>
+                        <label><b>SSN</b></label>
+                      <Search
+                        name="P2"
+                        loading={isLoading}
+                        onResultSelect={this.handleResultSelect}
+                        onSearchChange={_.debounce(this.handleSearchChange, 500, {
+                          leading: true,
+                        })}
+                        noResultsMessage = "No Parent Found."
+                        //minCharacters = "4" //minimum characters to show options
+                        results={results}
+                        value={p2_value}
+                        resultRenderer={resultRenderer}
+                        {...this.props}
+                      />
+                      
+                      {(!this.state.idP2.trim() == "") && <h5 style = {{color:'#68af64' }}><Icon name='check' />Details of this parent are known</h5>}
+                    </Grid.Column>
+                  </Grid>
+                  </Form.Field>
+
+
     
 {/*                   <Form.Field>
                     <label> 
@@ -227,35 +254,37 @@ export class FormParentDetails extends Component {
                  </Form.Field>
  */}
 
+              {(this.state.idP2.trim() == '') &&  
+                <>
+                <Form.Group widths='equal'>
+
                   <Form.Input
                     fluid icon='envelope' iconPosition='left'
                     label='E-mail address'type='email'placeholder='E-mail address'
-                    disabled={this.state.P2exists}
                     name="p2_Email"
                     defaultValue = {values.p2_Email}
                     onChange={handleChange('p2_Email')}
                   />
-                </Form.Group>
 
-                <Form.Group widths='equal'>
                   <Form.Input
                     label='First Name' placeholder='First Name'
                     name='p2_FirstName'
-                    disabled={this.state.P2exists}
                     defaultValue = {values.p2_FirstName}
                     onChange={handleChange('p2_FirstName')}
                   />
                   <Form.Input
                     label='Last Name' placeholder='Last Name'
                     name='p2_LastName'
-                    disabled={this.state.P2exists}
                     defaultValue = {values.p2_LastName}
                     onChange={handleChange('p2_LastName')}
                   />
                 </Form.Group>
                 </>
+              }
 
-            </Accordion.Content>
+            
+
+            </></Accordion.Content>
             </Accordion>
 
             <div className="addStudButtonField">
