d1d04c7c4e2eb63d50773fffc768d4a0d60da2af
renato-dl <s263092@studenti.polito.it>
2019-12-02 20:33:12 +0100

    Password bug fix

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -36,7 +36,6 @@ class AdminController extends BaseController {
       req.body.SSN, 
       password
     );
-    this.sendEmailToUser(req.body.eMail, password, req.body.firstName, req.body.lastName);
     res.send(parent); 
   }
 
@@ -46,19 +45,20 @@ class AdminController extends BaseController {
     let parent2;
     let parent1Insert = false;
     let parent2Insert = false;
+    let parent1Password, parent2password = null;
 
     if (!req.body.hasOwnProperty('firstParent')) {
       res.status(422).send({ success: false, error: 'Missing first parent' });
       return;
     } else {
       if (!req.body.firstParent.hasOwnProperty('ID')) {
-        const password = genRandomString(8);
+        parent1Password = genRandomString(8);
         parent1 = (await User.insertParentData(
           req.body.firstParent.FirstName,
           req.body.firstParent.LastName,
           req.body.firstParent.Email,
           req.body.firstParent.SSN,
-          password
+          parent1Password
         )).id;
         parent1Insert = true;
       } else {
@@ -68,13 +68,13 @@ class AdminController extends BaseController {
     }
     if (req.body.hasOwnProperty('secondParent')) {
       if (!req.body.secondParent.hasOwnProperty('ID')) {
-        const password = genRandomString(8);
+        parent2Password = genRandomString(8);
         parent2 = (await User.insertParentData(
           req.body.secondParent.FirstName,
           req.body.secondParent.LastName,
           req.body.secondParent.Email,
           req.body.secondParent.SSN,
-          password
+          parent2Password
         )).id;
         parent2Insert = true;
       } else {
@@ -96,16 +96,17 @@ class AdminController extends BaseController {
       );
 
       if(parent1Insert) {
-        this.sendEmailToUser(req.body.firstParent.Email, password, req.body.firstParent.FirstName, req.body.firstParent.LastName);
+        this.sendEmailToUser(req.body.firstParent.Email, parent1Password, req.body.firstParent.FirstName, req.body.firstParent.LastName);
       }
 
       if (parent2Insert) {
-        this.sendEmailToUser(req.body.secondParent.Email, password, req.body.secondParent.FirstName, req.body.secondParent.LastName);
+        this.sendEmailToUser(req.body.secondParent.Email, parent2Password, req.body.secondParent.FirstName, req.body.secondParent.LastName);
       }
 
       res.send({success:true, id: result.id});
 
     } catch(error) {
+      console.log(error)
       if (parent1Insert) {
         await User.remove(parent1);
       }
