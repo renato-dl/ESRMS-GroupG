fb995aaf0b811ae845e2ca5cc0b1051a576f5d0e
Tarzan Prenga <tarzanprenga@hotmail.com>
2019-12-03 10:10:52 +0100

    Updated links of FE

--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Menu, Segment, Sidebar } from 'semantic-ui-react';
 import '../../assets/styles/global.scss';
 
@@ -6,9 +6,11 @@ import { ParentMenu } from './Menus/ParentMenu';
 import { TeacherMenu } from './Menus/TeacherMenu';
 import { AdminMenu } from './Menus/AdminMenu';
 import { SysAdminMenu } from './Menus/SysAdminMenu';
+import { ApplicationStoreContext } from '../../store';
 
 
 export const AppSidebar = (props) => {
+  const context = useContext(ApplicationStoreContext);
   const loc = window.location.pathname;
   
   return (
@@ -21,11 +23,9 @@ export const AppSidebar = (props) => {
         sidebar = "true"
         menu = "true"
         visible = {props.visibility}
-        //onHide = 
-        //onVisible
       >
 
-        {loc.indexOf("/parent") !== -1 && <ParentMenu />}
+        {loc.indexOf("/parent") !== -1 && <ParentMenu {...context.state.parent} />}
         {loc.indexOf("/teacher") !== -1 && <TeacherMenu />}
         {loc.indexOf("/admin") !== -1 && <AdminMenu />}
         {loc.indexOf("/sysadmin") !== -1 && <SysAdminMenu />}
