7224fd0596b09b3f5beaa21728708bfc4ad9037d
Xileny <xileny.seijasp@gmail.com>
2019-12-06 16:30:48 +0100

    class composition name changed

--- frontend/src/containers/Admin/Admin.js
+++ frontend/src/containers/Admin/Admin.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Admin.scss';
-import { ClassComposition } from '../../components/class_composition/class_composition';
+import { ClassComposition } from '../../components/ClassComposition/ClassComposition';
 export class Admin extends React.Component{
     render() {
     return (
