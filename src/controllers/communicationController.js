617045eb7ffd6e4b8cfbc3cefd28c353da93ad81
Tarzan Prenga <tarzanprenga@hotmail.com>
2019-12-10 10:59:55 +0100

    Updated  communicat controller methods and model

--- src/controllers/communicationController.js
+++ src/controllers/communicationController.js
@@ -5,22 +5,27 @@ class CommunicationController extends BaseController {
   
   async list(req, res) {
     const communications = await CommunicationModel.findAll();
-    res.send({communications});
+    res.send({ communications });
   }
 
   async add(req, res) {
     const communication = await CommunicationModel.add(req.body.title, req.body.description);
-    res.send({communication});
+    res.send({ communication });
   }
 
   async update(req, res) {
     const communication = await CommunicationModel.update(req.params.id, req.body.title, req.body.description);
-    res.send({communication});
+
+    if (!communication) {
+      return res.send({ success: false, message: "Couldn't update communication. Please try again later." });
+    }
+
+    res.send({ success: true, communication });
   }
 
   async remove(req, res) {
     await CommunicationModel.remove(req.params.id);
-    res.send({success: true});
+    res.send({ success: true });
   }
 
 }
--- src/database/models/communication.js
+++ src/database/models/communication.js
@@ -5,16 +5,45 @@ class Communication extends Model {
     super('Communications');
   }
 
-  async add(title, description) {
+  validteTitleAndDescription(title, description) {
+    if (!title || !description) {
+      throw new Error('Please provide a valid title and description.');
+    }
 
+    if (title.length > 255) {
+      throw new Error('Invalid title.');
+    }
   }
 
-  async update(id, title, description) {
+  async add(Title, Description) {
+    this.validteTitleAndDescription(Title, Description);
+
+    const communicationID = await this.create({ Title, Description });
+    return await this.findById(communicationID);
+  }
+
+  async update(id, Title, Description) {
+    if (!id) {
+      throw new Error('Please provide a valid id.');
+    }
+
+    this.validteTitleAndDescription(Title, Description);
+
+    const updated = await super.update(id, { Title, Description });
     
+    if (updated) {
+      return await super.findById(id);
+    }
+
+    return null;
   }
 
   async remove(id) {
-    
+    if (!id) {
+      throw new Error('Please provide a valid id.');
+    }
+
+    return super.remove(id);
   }
 }
 
