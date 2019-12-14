e9c9951cd733f2baf558a95877640cac9d327891
erxian <15221617758@163.com>
2019-12-14 10:39:24 +0100

    remove console log

--- frontend/src/components/ChildAttendance/ChildAttendance.js
+++ frontend/src/components/ChildAttendance/ChildAttendance.js
@@ -25,7 +25,6 @@ export class ChildAttendance extends React.Component{
 
     async componentDidMount() {
         await this.onNavigate(new Date());
-        console.log(this.state.attendanceForCalendar)
       }
     
     fetchAttendance = async (from, to) => {
