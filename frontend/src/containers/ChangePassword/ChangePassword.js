49bea4b006c2692f5b86bec65de10d6e2acda386
Mara-M <maramarmaryan@gmail.com>
2019-12-21 00:11:43 +0100

    validation

--- frontend/src/containers/ChangePassword/ChangePassword.js
+++ frontend/src/containers/ChangePassword/ChangePassword.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { api } from '../../services/api';
-import { Button, Form, Grid, Header, Icon, Dimmer, Loader, Segment, Container } from 'semantic-ui-react'
-import validator from 'validator';
+import { Button, Form, Grid, Header, Icon, Segment, Container } from 'semantic-ui-react'
+import passwordValidator from 'password-validator';
 
 import '../Login/Login.scss';
 
@@ -20,7 +20,7 @@ export class ChangePassword extends React.Component {
     componentDidMount() {
         try {
           const role = localStorage.getItem('role');
-          console.log(role);
+          //console.log(role);
     
           if (role) {
             this.setState({ showChangePass: true });
@@ -38,13 +38,28 @@ export class ChangePassword extends React.Component {
         this.setState({[name]: value});
     };
 
+    validatePassword(password){
+        let schema = new passwordValidator();
+        schema
+        .is().min(8)                                    // Minimum length 8
+        .is().max(100)                                  // Maximum length 100
+        .has().uppercase()                              // Must have uppercase letters
+        .has().lowercase()                              // Must have lowercase letters
+        .has().digits()                                 // Must have digits
+        .has().not().spaces()                           // Should not have spaces
+        return schema.validate(password);
+    }
 
     validateFields = () => {
         let errors = this.state.errors;
     
         errors['oldPassword'] = !this.state.oldPassword.length;
-        errors['newPassword'] = !this.state.newPassword.length;
-        errors['newPasswordConfirm'] = !this.state.newPasswordConfirm.length;
+        errors['newPassword'] = !this.validatePassword(this.state.newPassword);
+        errors['newPasswordConfirm'] = !(this.state.newPasswordConfirm === this.state.newPassword && this.state.newPasswordConfirm.length);
+
+        if (this.state.newPasswordConfirm.length) { errors['weakMsg'] = errors['newPassword']; }
+        if (this.state.newPassword.length) {errors['dmatchMsg'] = errors['newPasswordConfirm'];}
+        errors['sameMsg'] = (this.state.oldPassword === this.state.newPassword && this.state.oldPassword.length != 0);
         
         const hasErrors = !!Object.keys(errors).filter((e) => errors[e]).length;
         return [hasErrors, errors];
@@ -60,15 +75,16 @@ export class ChangePassword extends React.Component {
     submitPassChange = async () => {
         this.setState({
             showErrMsg:false,
-            errors:{}
+            errors:{},
         });
 
-       // const [hasErrors, errors] = this.validateFields();
-        /* if (hasErrors) {
+        const [hasErrors, errors] = this.validateFields();
+        if (hasErrors) {
             this.setState({errors});
             return;
-        } */
+        } 
 
+        this.setState({loader: true})
         try{
             const dat = {
                 oldPass :this.state.oldPassword,
@@ -76,17 +92,15 @@ export class ChangePassword extends React.Component {
             };
 
             const response = await api.auth.changePassword(dat);
-            console.log(response);
             if(response.data.success){
                 //showw success message .. fake loading 1-2 sec
-                this.setState({loader: 'true'})
-                setTimeout(this.redirectToLogin, 1000);
+                setTimeout(this.redirectToLogin, 500);
             }
             
 
         }catch (e) {
-            this.setState({showErrMsg: true});
-            console.log("Login: " + e);
+            this.setState({loader: false, showErrMsg: true});
+            console.log("ChangePass: " + e);
             return;
         }
     }
@@ -94,34 +108,29 @@ export class ChangePassword extends React.Component {
     render() {
         return (
                 <>
-                <Dimmer active={this.state.loader}>
-                    <>
-                    <Loader size='middle' style={{color:'#f7f7f7'}}>
-                        <h3 style={{color:'#f7f7f7'}}>Updating password...</h3>
-                    </Loader>
-                    </>
-                </Dimmer>
-                <Dimmer active={this.state.successMsg}>
-                    <>
-                    <h2 style={{color:'#008230'}}><Icon name="check circle"/>Your password has been changed successfully!</h2> 
-                    <h3 style={{color:'#f7f7f7'}}>Click here to <a href="/login"><u>Log in with your new password.</u></a></h3>
-                    </>
-                </Dimmer>
-                
                 <div className="loginBackground"></div>
                 <Container>
-                {this.state.showChangePass && !this.state.loader && !this.state.successMsg &&
+                {this.state.successMsg &&
+                    <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+                        <Grid.Column >
+                    <h2 style={{color:'#008230'}}><Icon name="check circle"/>Your password has been changed successfully!</h2> 
+                    <h3><a href="/login"><u>Click here</u></a> to log in with your new password.</h3>
+                    </Grid.Column>
+                    </Grid>
+                }
+                {this.state.showChangePass && !this.state.successMsg &&
                     <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
                         <Grid.Column className="loginContainer" >
                         <Header as='h2'  textAlign='center' className="loginHeader">
                             <Icon name="unlock alternate"/>Change Password 
                         </Header>
                         
-                        <Form size='large'>
+                        <Form size='large' loading={this.state.loader}>
                             <Segment stacked>
 
                             <Form.Input
                                 fluid
+                                required
                                 className="chpwdInput"
                                 placeholder='Current password'
                                 type='password'
@@ -133,6 +142,7 @@ export class ChangePassword extends React.Component {
 
                             <Form.Input
                                 fluid
+                                required
                                 className="chpwdInput"
                                 placeholder='New password'
                                 type='password'
@@ -144,6 +154,7 @@ export class ChangePassword extends React.Component {
                             
                             <Form.Input
                                 fluid
+                                required
                                 className="chpwdInput"
                                 placeholder='Retype new password'
                                 type='password'
@@ -152,11 +163,14 @@ export class ChangePassword extends React.Component {
                                 value={this.state.newPasswordConfirm}
                                 onChange={this.handleInputChange}
                             />
+                            {this.state.errors['dmatchMsg'] &&  <p className="invMsg">New passwords do not match.</p>}
+                            {this.state.errors['weakMsg'] && <p className="invMsg">Please choose a more secure password. <br/> It should be at least 8 characters with uppercase, lowercase lettes and digits. Should not contain spaces.</p>}
+                            {this.state.errors['sameMsg'] && <p className="invMsg">You have entered the same current password.</p>}
                             
                             {this.state.showErrMsg && 
-                            <p className="errMsg">
+                            <p className="invMsg">
                                 <Icon name="exclamation triangle"/>
-                                Credentials are invalid.
+                                Inserted current password is invalid.
                             </p>}
 
                             <Button fluid size='large' className = "loginBtn" onClick={this.submitPassChange}>
