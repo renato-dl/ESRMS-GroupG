67bc8a61cad8e9fdacbe06a23de331ecee4c63f6
Tarzan Prenga <tarzanprenga@hotmail.com>
2020-01-01 12:47:07 +0100

    Added support material migration and model

---
+++ src/database/models/supportMaterial.js
@@ -0,0 +1,12 @@
+import {Model} from './base';
+import moment from 'moment';
+import File from './File';
+
+class SupportMaterial extends Model {
+  constructor() {
+    super('Support_Material');
+  }
+
+}
+
+export default new SupportMaterial();
