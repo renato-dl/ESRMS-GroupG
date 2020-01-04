a3d69fc26b42767fbb09df0346ee7e474a35feff
renato-dl <s263092@studenti.polito.it>
2020-01-04 13:19:09 +0100

    Subject router and controller

---
+++ src/controllers/subjectController.js
@@ -0,0 +1,12 @@
+import {BaseController} from "./baseController";
+import Subject from '../database/models/subject'
+
+class SubjectController extends BaseController {
+
+  async listAll(req, res) {
+    const subjects = await Subject.findAll();
+    res.send(subjects);
+  }
+}
+
+export default new SubjectController();
