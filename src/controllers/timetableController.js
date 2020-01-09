c0304f9a0fa92f7d49d8c7014829c1af557f2cb6
Tarzan Prenga <tarzanprenga@hotmail.com>
2020-01-09 14:07:36 +0100

    Implemented method to list all the created timetables

--- src/controllers/timetableController.js
+++ src/controllers/timetableController.js
@@ -4,13 +4,13 @@ import TimetableModel from "../database/models/timetable";
 class TimetableController extends BaseController {
   
   async list(req, res) {
-    const timetables = await TimetableModel.findAll();
+    const timetables = await TimetableModel.list();
     res.send({ timetables });
   }
 
   async add(req, res) {
     const timetable = await TimetableModel.add(req.body);
-    res.send({ timetable });
+    res.send(timetable);
   }
 
   async remove(req, res) {
