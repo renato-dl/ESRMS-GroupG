925cd7e4a04a43a6a08f47959a803bd089934b2d
Tarzan Prenga <tarzanprenga@hotmail.com>
2020-01-09 18:27:20 +0100

    Added method to save timetable for class

--- src/database/models/timetable.js
+++ src/database/models/timetable.js
@@ -1,8 +1,17 @@
 import {Model} from './base';
+import Class from '../models/class';
 import {
-  groupBy, 
-  pick
+  groupBy,
+  sortBy,
+  pick,
+  isArray,
+  isInteger,
+  isString,
+  isEqual
 } from 'lodash';
+
+const DEFAULT_HOURS = [8, 9, 10, 11, 12, 13];
+const DEFAULT_DAYS = [1, 2, 3, 4, 5];
 
 class Timetable extends Model {
   constructor() {
@@ -35,8 +44,110 @@ class Timetable extends Model {
     return [];
   }
 
-  async add(data) {
-    return {};
+  async add(classId, timetable) {
+    if (!classId) {
+      throw new Error('Missing or invalid classId.');
+    }
+
+    if (!timetable) {
+      throw new Error('Missing or timetable.');
+    }
+
+    if (!isArray(timetable)) {
+      throw new Error('Invalid timetable type.')
+    }
+
+    if (!timetable.length) {
+      throw new Error('Empty timetable.')
+    }
+
+    const allowedDays = [...DEFAULT_DAYS];
+    let allowedHours = [...DEFAULT_HOURS];
+
+    // check if all days are passed
+    const daysReceived = [...new Set(timetable.map((entry) => entry.day))];
+    if (!isEqual(sortBy(allowedDays), sortBy(daysReceived))) {
+      throw new Error('Incomplete timetable.')
+    }
+
+    // check hours
+    const groupedByDay = groupBy(timetable, 'day');
+    Object.keys(groupedByDay).forEach((day) => {
+      const dayHours = groupedByDay[day];
+      // check if all time slots are passed
+      const hoursReceived = [...new Set(dayHours.map((entry) => {
+        if (isInteger(entry.hour)) {
+          return entry.hour;
+        }
+
+        if (isString(entry.hour)) {
+          return Number(entry.hour.charAt(0));
+        }
+
+        return 0;
+      }))];
+
+      if (hoursReceived.length > allowedHours.length) {
+        throw new Error('Invalid timetable hours.');
+      }
+      
+      // case when the last hour (13) is not proviced
+      if (hoursReceived.length < allowedHours.length) {
+        allowedHours.pop();
+      }
+
+      if (!isEqual(sortBy(allowedHours), sortBy(hoursReceived))) {
+        throw new Error('Invalid timetable hours.');
+      }
+
+      allowedHours = [...DEFAULT_HOURS];
+    });
+
+    const subjectsRecieved = [...new Set(timetable.map((entry) => entry.subjectId))];
+    if (!subjectsRecieved.length) {
+      throw new Error('Invalid subjects.');
+    }
+
+    const connection = await this.db.getConnection();
+    const subjectIds = subjectsRecieved.map((ID) => "'" + ID + "'").join(',');
+    const subjects = await connection.query(`SELECT * FROM Subjects WHERE Subjects.ID IN (${subjectIds})`);
+    if (subjects.length !== subjectsRecieved.length) {
+      connection.release();
+      throw new Error('Invalid subjects.');
+    }
+
+    let currentClass = null;
+    try {
+      currentClass = await Class.findById(classId);
+    } catch(e) {
+      connection.release();
+      throw new Error('Class not found.');
+    }
+
+    const currentTimetable = await connection.query(`SELECT * FROM ${this.tableName} WHERE ClassId = ? LIMIT 1`, [currentClass.ID]);
+    if (currentTimetable.length) {
+      throw new Error('Class already has a timetable.');
+    }
+
+    const parsedTimetableData = timetable.map((entry) => {
+      return {
+        ClassId: classId,
+        SubjectId: entry.subjectId,
+        Day: entry.day,
+        Hour: '' + entry.hour
+      }
+    });
+
+    let results = [];
+    try {
+      results = await this.createMany(parsedTimetableData);
+    } catch(e) {
+      connection.release();
+      throw new Error('Invalid timetable contents.');
+    }
+    
+    connection.release();
+    return !!results.length;
   }
 
   async remove(classId) {
